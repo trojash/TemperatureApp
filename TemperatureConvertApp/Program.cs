e9d4cc463486306797f9b31e8d312a8a641353aa
trojash <tonyestuardor@outlook.com>
2026-09-07 18:31:24 -0600

    First commit

---
+++ TemperatureConvertApp/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+Console.WriteLine("This is a convert temperature app\n");
+int fahrenheit = 94;
+decimal celsius = 34;
+
+decimal convertToCelsius = (fahrenheit - 32m) * (5m/9m);
+decimal convertTofahrenheit = (celsius * 9m / 5m) + 32;
+
+Console.WriteLine($"The temperature in celsius is: {convertToCelsius} °C");
+Console.WriteLine($"The temperature in fahrenheit is: {convertTofahrenheit} °F");
+
